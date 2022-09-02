71445bc99705ce50ed7724b513b22be18d060f60
seo-yeongjun <zanygeek8371@gmail.com>
2022-09-02 20:33:07 +0900

    first initialization

---
+++ src/main/java/com/projectjy/projectjybackend/ProjectJyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.projectjy.projectjybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectJyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projectjy/projectjybackend/ProjectJyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectjy.projectjybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
